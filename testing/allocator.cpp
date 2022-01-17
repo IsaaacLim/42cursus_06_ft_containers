16f300d66fcc6def6d28bd478a4df644117c7866
isaaclim <isaaclimjj@yahoo.com>
2022-01-17 17:06:18 +0800

    added comments for WSL performance

--- testing/allocator.cpp
+++ testing/allocator.cpp
@@ -14,22 +14,22 @@ int main()
 		std::allocator<int> alloc;
 		int *p;
 
-		p = alloc.allocate(1); // space for one int
-		alloc.deallocate(p, 1);		// remove space for one int
+		p = alloc.allocate(1);	// space for one int
+		alloc.deallocate(p, 1); // remove space for one int
 
-		p = alloc.allocate(5);	// space for five ints
+		p = alloc.allocate(5);		// space for five ints
 		alloc.construct(p, 100);	// construct the 1st index;
 		p[1] = 10;					// construct the 2nd index;
-		alloc.construct(p + 2, 50);	// construct the 3rd index;
-		p[6] = 999;					// ??????
+		alloc.construct(p + 2, 50); // construct the 3rd index;
+		// p[6] = 999;					// still works on Mac but not WSL
 
-		alloc.destroy(p + 1);		// ??????
+		alloc.destroy(p + 1); // ??????
 
 		std::cout << "p[0]: " << *p << '\n';
 		std::cout << "p[1]: " << p[1] << '\n';
 		std::cout << "p[2]: " << *(p + 2) << '\n';
-		std::cout << "p[6]: " << p[6] << " <- still works????\n"; // ??????
-		alloc.deallocate(p, 5);		// remove space for five ints
+		// std::cout << "p[6]: " << p[6] << " <- still works????\n"; // on WSL, will give random number
+		alloc.deallocate(p, 5); // remove space for five ints
 	}
 	// default allocator for strings
 	{
@@ -42,14 +42,14 @@ int main()
 
 		alloc.destroy(str + 1); // ??????
 
-		std:: cout << str[0] << *(str + 1) << str[2] << '\n';
+		std::cout << str[0] << *(str + 1) << str[2] << '\n';
 
 		//destroy 3 strings
 		alloc.destroy(str);
 		alloc.destroy(str + 1);
 		alloc.destroy(str + 2);
 
-		alloc.deallocate(str, 3);		// deallocate space for 3 strings
+		alloc.deallocate(str, 3); // deallocate space for 3 strings
 	}
 	// system("leaks a.out");
 }
